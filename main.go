3c4d74f4006b55db761e79e8a5c8c85571823c14
dropk1ck <kwmcmullen@gmail.com>
2020-04-12 11:21:56 -0400

    added cat-file cmd, currently reads serialized obj to stdout

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     "crypto/sha1"
     "encoding/hex"
     "fmt"
+    "io"
     "io/ioutil"
     "log"
     "os"
@@ -14,8 +15,8 @@ import (
     "gopkg.in/ini.v1"
 )
 
-// the basic object in git. a type, contents, and size of the contents.
-// really just a TLV with extra bytes between them
+// the basic object in git: a type, contents, and size of the contents.
+// really just a TLV with extra bytes between them (when the object is serialized)
 type Object struct {
     Type string
     Size uint64
@@ -30,17 +31,13 @@ func NewObject(objtype string, contents string) *Object {
     return obj
 }
 
-// git object:
-//   - take sha1 sum of contents
-//   - first 2 bytes are dirname
-//   - remaining bytes are filename in dir
-//   - object format:
+// serializing a git object:
+//   - serialized object format:
 //     - header (blob, commit, tag, tree)
 //     - ASCII space (0x20)
 //     - size of object in bytes as ASCII number
 //     - NULL (0x00)
 //     - object contents
-//   - objects are stored *compressed* with zlib
 
 func (obj Object) Serialize() []byte {
     var b bytes.Buffer
@@ -53,6 +50,12 @@ func (obj Object) Serialize() []byte {
     b.WriteString(obj.Contents)
     return b.Bytes()
 }
+
+// stores a serialized git object to the repository
+//   - take sha1 sum of contents
+//   - first 2 bytes are dirname
+//   - remaining bytes are filename in dir
+//   - objects are stored *compressed* with zlib
 
 func WriteObjectToFile(objBytes []byte, sha1Sum []byte) {
     // compress the bytes into a buffer
@@ -71,6 +74,25 @@ func WriteObjectToFile(objBytes []byte, sha1Sum []byte) {
     }
 }
 
+func ReadObjectFromFile(sha1Hash string) {
+    //var contents bytes.Buffer
+
+    filePath := ".git/objects/" + sha1Hash[0:2] + "/" + sha1Hash[2:]
+    zContents, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        log.Fatalf("ERROR: cannot read file %s - %s", filePath, err)
+    }
+
+    // contents are compress with zlib, uncompress
+    zReader := bytes.NewReader(zContents)
+    r, err := zlib.NewReader(zReader)
+    if err != nil {
+        log.Fatalf("ERROR: could not decompress contents - %s", err)
+    }
+    io.Copy(os.Stdout, r)
+    r.Close()
+}
+
 func HashObject(path string, storeObject bool, tag string) {
     // suck file contents into memory
     contents, err := ioutil.ReadFile(path)
@@ -87,7 +109,7 @@ func HashObject(path string, storeObject bool, tag string) {
     h.Write(objBytes)
     sha1sum := h.Sum(nil)
 
-    fmt.Printf("Serialized object SHA1 hash: %x\n", sha1sum)
+    fmt.Printf("object SHA1 hash: %x\n", sha1sum)
 
     // optionally write object to repo, compressing with zlib
     if (storeObject) {
@@ -157,6 +179,8 @@ func main() {
     hashObjStore := hashObjCmd.Bool("w", false, "write object to repository")
     hashObjTag := hashObjCmd.String("t", "blob", "type of object tag")
 
+    catFileCmd := flag.NewFlagSet("cat-file", flag.ExitOnError)
+
     if len(os.Args) < 2 {
         fmt.Println("no subcommand specified")
         os.Exit(1)
@@ -175,6 +199,15 @@ func main() {
         }
         path := args[0]
         HashObject(path, *hashObjStore, *hashObjTag)
+    case "cat-file":
+        catFileCmd.Parse(os.Args[2:])
+        args := catFileCmd.Args()
+        if len(args) < 2 {
+            fmt.Println("cat-file: expected type and object SHA1 hash")
+            os.Exit(1)
+        }
+        sha1Hash := args[1]
+        ReadObjectFromFile(sha1Hash)
     default:
         fmt.Println("unknown subcommand: %s", subcommand)
     }
